08e8be71979440774e056562b8c6a8c2f4d2fed2
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-05-17 14:22:53 +0300

    sss

--- Common/Sharepoint.cs
+++ Common/Sharepoint.cs
@@ -165,7 +165,7 @@ namespace Common
 
                     byte[] bytes = System.IO.File.ReadAllBytes(pdfPath);
                     MemoryStream mStream = new MemoryStream(bytes);
-                    //AttachmentCreationInformation aci = new AttachmentCreationInformation();
+                    AttachmentCreationInformation aci = new AttachmentCreationInformation();
                     //aci.ContentStream = mStream;
                     //aci.FileName = Path.GetFileName(pdfPath);// "AttachmentFile"; // attachEntity.FileName;
                     //Attachment attachment = oListItem.AttachmentFiles.Add(aci);
