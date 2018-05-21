015a4a92b4553ac192cbe5154f58a8f0c8cbb272
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-05-21 11:22:31 +0300

    Test Attachment2

--- Common/Sharepoint.cs
+++ Common/Sharepoint.cs
@@ -282,6 +282,7 @@ namespace Common
                 //    Context.Load(attachment);
                 //    Context.ExecuteQuery();
                 //    fileStream.Close();
+
                 //}
             }
         }
