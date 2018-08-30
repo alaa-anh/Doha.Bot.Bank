8835e7e4ff284447c0d508ae3738f9986ea1138e
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-08-30 11:51:20 +0300

    new test

--- Doha.Bot.Bank/Controllers/BankBotTeamController.cs
+++ Doha.Bot.Bank/Controllers/BankBotTeamController.cs
@@ -50,9 +50,7 @@ namespace Doha.Bot.Bank
             {
                 // Handle knowing tha the user is typing
             }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
+            
 
             return null;
         }
